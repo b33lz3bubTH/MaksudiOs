38fa672f30926f485f82810adb52e752c64ef854
Sourav Ahmed <souravsunju@gmail.com>
2022-07-14 22:30:00 +0530

    bios parameter block

--- maksudiKernel_v1.0/src/boot.asm
+++ maksudiKernel_v1.0/src/boot.asm
@@ -12,10 +12,31 @@
 ; No Mem Security/Hardware Security
 ; only access 8 and 16 bit registers
 
-ORG 0x7c00
+ORG 0
 BITS 16
 
+_bootstraping:
+    ; real hardware is madarchod BIOS parameter block, real hardware tries to modify the data which can corrupt.
+    ; cause with usb bootup, bios madarchod treats it as hdd which leads to coruption, not exactly but similar shit
+    ; not all bios is madarchod, but mostly mine is
+    jmp short start
+    nop
+
+times 33 db 0   ; prevent writing somthing to our program [This is BIOS Parameter Block]
+
+
 start:
+    jmp 0x7c0:init         ;this will set the code segment to 0x7c0
+
+init:
+    cli ;clear interupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00   
+    sti ;enable interupts
 
     mov si, bootloaderInit
     call __printF__
